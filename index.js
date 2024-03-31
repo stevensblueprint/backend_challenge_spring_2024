be9b457d69cb4f901e8c257d944d712a9e40fdb0
ayushmisra05 <140655885+ayushmisra05@users.noreply.github.com>
2024-03-31 19:23:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,99 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const mongoose = require('mongoose');
+const Volunteer = require('./volunteer.model.js');
+const app = express()
 
-app.get("/api/volunteers", (req, res) => {
-  res.send("Implement");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+app.get('/', (req, res) => {
+    res.send("Hello from Node API");
 });
 
-app.post("/api/volunteers", (req, res) => {
-  res.send("Implement");
+//POST VOLUNTEER
+app.post('/api/volunteers', async (req, res) => {
+    try {
+        const volunteer = await Volunteer.create(req.body);
+        res.status(200).json(volunteer);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
 
-app.get("/api/volunteers/:volunteerId", (req, res) => {
-  res.send("Implement");
+//GET VOLUNTEER
+app.get('/api/volunteers', async (req, res) => {
+
+    try {
+        const volunteers = await Volunteer.find({});
+        res.status(200).json(volunteers);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+
 });
 
-app.put("/api/volunteers/:volunteerId", (req, res) => {
-  res.send("Implement");
+//GET ID
+app.get('/api/volunteer/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const volunteer = await Volunteer.findById(id);
+        res.status(200).json(volunteer);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
 
-app.delete("/api/volunteers/:volunteerId", (req, res) => {
-  res.send("Implement");
+//UPDATE VOLUNTEER (ID)
+app.put('/api/volunteer/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const volunteer = await Volunteer.findByIdAndUpdate(id, req.body);
+
+        if (!volunteer) {
+            return res.status(404).json({ message: "Volunteer not found" });
+        }
+
+        const updatedVolunteer = await Volunteer.findById(id);
+        res.status(200).json(updatedVolunteer);
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
 
-app.get("/api/volunteers/:volunteerId/skills", (req, res) => {
-  res.send("Implement");
+// DELETE Volunteer
+app.delete("/api/volunteer/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const volunteer = await Volunteer.findByIdAndDelete(id);
+
+        if (!volunteer) {
+            return res.status(404).json({ message: "Volunteer not found" });
+        }
+
+        res.status(200).json({ message: "Volunteer deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
 
-app.post("/api/volunteers/:volunteerId/skills", (req, res) => {
-  res.send("Implement");
+
+//Web
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
 });
 
-app.delete("/api/volunteers/:volunteerId/skills/:skillId", (req, res) => {
-  res.send("Implement");
-});
+mongoose.connect("mongodb+srv://ayushmisra71:Omshanti%4005@backenddb.cdivndj.mongodb.net/backendDB?retryWrites=true&w=majority&appName=BackendDB")
+    .then(() => {
+        console.log("Connected to database!");
+    })
+    .catch((error) => {
+        console.log("Connection failed!", error);
 
-app.get("/api/volunteers/:volunteerId/events", (req, res) => {
-  res.send("Implement");
-});
+    })
 
-app.post("/api/events/{eventId}/volunteers/:volunteerId", (req, res) => {
-  res.send("Implement");
-});
 
-app.delete("/api/events/{eventId}/volunteers/:volunteerId", (req, res) => {
-  res.send("Implement");
-});
-
-app.listen(3000, () => console.log("Server is running on port 3000"));
